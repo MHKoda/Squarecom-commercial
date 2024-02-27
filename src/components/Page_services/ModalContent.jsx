9bc557a2dac10f150d588b4ea83f79f92bd501de
MHKoda <malo.houbre@gmail.com>
2024-02-27 16:03:54 +0100

    oui

--- src/components/Page_services/ModalContent.jsx
+++ src/components/Page_services/ModalContent.jsx
@@ -27,37 +27,38 @@ const ModalContent = ({ closeModal }) => {
     }
 
     return (
-        <div className="modal-proj overlay" onClick={closeModal} onKeyDown={handleKeyDown} tabIndex={0}>
-            <div className='bandeau-top'>
-                <h1 className='h1-modale'>INFORMATIONS COMPL&#201;MENTAIRES</h1>
-                <button className="close-modal">X</button>
-            </div>
-            <div className="modale-content">
-                <div className="thumbnail-proj-modal">
-                    <img src='../medias/cardf.jpg' alt='cardf' />
+        <div className="modal-proj overlay" onClick={closeModal} onKeyDown={handleKeyDown}>
+            <input type="text" />
+                <div className='bandeau-top'>
+                    <h1 className='h1-modale'>INFORMATIONS COMPL&#201;MENTAIRES</h1>
+                    <button className="close-modal">X</button>
                 </div>
-                <div className="descriptif-proj">
-                    <ul>
-                        <li className='nom-client'>
-                            <h2 className='titre_desc'>Nom du client :</h2>
-                            <p className='content_desc'>Car'DF</p>
-                        </li>
-                        <li className='support-util'>
-                            <h2 className='titre_desc'>Support utilisé :</h2>
-                            <p className='content_desc'>Wordpress</p>
-                        </li>
-                        <li className='format-proj'>
-                            <h2 className='titre_desc'>Format :</h2>
-                            <p className='content_desc'>Site internet</p>
-                        </li>
-                        <li className='method-proj'>
-                            <h2 className='titre_desc'>Méthode d'impression :</h2>
-                            <p className='content_desc'>Ctrl + P</p>
-                        </li>
-                    </ul>
+                <div className="modale-content">
+                    <div className="thumbnail-proj-modal">
+                        <img src='../medias/cardf.jpg' alt='cardf' />
+                    </div>
+                    <div className="descriptif-proj">
+                        <ul>
+                            <li className='nom-client'>
+                                <h2 className='titre_desc'>Nom du client :</h2>
+                                <p className='content_desc'>Car'DF</p>
+                            </li>
+                            <li className='support-util'>
+                                <h2 className='titre_desc'>Support utilisé :</h2>
+                                <p className='content_desc'>Wordpress</p>
+                            </li>
+                            <li className='format-proj'>
+                                <h2 className='titre_desc'>Format :</h2>
+                                <p className='content_desc'>Site internet</p>
+                            </li>
+                            <li className='method-proj'>
+                                <h2 className='titre_desc'>Méthode d'impression :</h2>
+                                <p className='content_desc'>Ctrl + P</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
 
